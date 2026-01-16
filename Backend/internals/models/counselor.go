da09ca47b3bb18c6cd13ffc61e36d04a99bb1d9c
RoshithBalachandran <roshith9745@gmail.com>
2026-01-16 19:42:38 +0530

    Counselor model updated

--- Backend/internals/models/counselor.go
+++ Backend/internals/models/counselor.go
@@ -3,10 +3,10 @@ package models
 type Counselor struct {
 	ID             uint   `gorm:"primaryKey"`
 	Name           string `gorm:"not null"`
-	Qualification  string
-	Specialization string
-	ProfileImage   string
+	Qualification  string `json:"qualification"`
+	Specialization string `json:"specialization"`
 	Email          string `json:"email"`
 	Password       string `json:"password"`
+	Block          bool   `json:"block"`
 	IsActive       bool   `gorm:"default:true"`
 }
