1c5c45dbabf846a0be1ea68f2416de40c7fa750a
RoshithBalachandran <roshith9745@gmail.com>
2026-01-16 19:43:54 +0530

    admin routes updated

--- Backend/routes/admin.go
+++ Backend/routes/admin.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	adminhandlers "github.com/insight/internals/AdminHandlers"
+	adminapi "github.com/insight/internals/adminApi"
 	"github.com/insight/internals/authHandler"
 	"github.com/insight/internals/middleware"
 )
@@ -19,7 +20,15 @@ func SetUpAdminRoutes(r *gin.Engine) {
 		admin.GET("/profile", adminhandlers.GetAdminProfile)
 		admin.PUT("/profile", adminhandlers.UpdateAdminProfile)
 
+		// User endpoints
+		admin.GET("/users", adminapi.AdminGetAllUsers)
+		admin.POST("/users", adminapi.AdminCreateUser)
+		admin.PUT("/users/:id", adminapi.AdminUpdateUser)
 
+		// Counselor endpoints
+		admin.GET("/counselors", adminapi.AdminGetAllCounselors)
+		admin.POST("/counselors", adminapi.AdminCreateCounselor)
+		admin.PUT("/counselors/:id", adminapi.AdminUpdateCounselor)
 
 		admin.POST("/logout", authHandler.Logout)
 	}
