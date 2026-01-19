233b3ec66066f28d9d985adb3c6e0f266156c47e
RoshithBalachandran <roshith9745@gmail.com>
2026-01-19 14:26:21 +0530

    routes updated

--- Backend/routes/admin.go
+++ Backend/routes/admin.go
@@ -3,7 +3,8 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	adminhandlers "github.com/insight/internals/AdminHandlers"
-	adminapi "github.com/insight/internals/adminApi"
+	counselor "github.com/insight/internals/adminApi/Counselor"
+	adminusers "github.com/insight/internals/adminApi/users"
 	"github.com/insight/internals/authHandler"
 	"github.com/insight/internals/middleware"
 )
@@ -18,17 +19,17 @@ func SetUpAdminRoutes(r *gin.Engine) {
 	admin.Use(middleware.AuthMiddleware(), middleware.AdminOnly())
 	{
 		admin.GET("/profile", adminhandlers.GetAdminProfile)
-		admin.PUT("/profile", adminhandlers.UpdateAdminProfile)
+		admin.PATCH("/profile", adminhandlers.UpdateAdminProfile)
 
 		// User endpoints
-		admin.GET("/users", adminapi.AdminGetAllUsers)
-		admin.POST("/users", adminapi.AdminCreateUser)
-		admin.PUT("/users/:id", adminapi.AdminUpdateUser)
+		admin.GET("/users", adminusers.AdminGetAllUsers)
+		admin.POST("/users", adminusers.AdminCreateUser)
+		admin.PUT("/users/:id", adminusers.AdminUpdateUser)
 
 		// Counselor endpoints
-		admin.GET("/counselors", adminapi.AdminGetAllCounselors)
-		admin.POST("/counselors", adminapi.AdminCreateCounselor)
-		admin.PUT("/counselors/:id", adminapi.AdminUpdateCounselor)
+		admin.GET("/counselors", counselor.AdminGetAllCounselors)
+		admin.POST("/counselors", counselor.AdminCreateCounselor)
+		admin.PUT("/counselors/:id", counselor.AdminUpdateCounselor)
 
 		admin.POST("/logout", authHandler.Logout)
 	}
